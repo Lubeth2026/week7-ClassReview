e87aa1b253652b68ae95d1677e9e43c7fb7121c8
Lubeth <lubethjimenez@yahoo.com>
2026-03-22 20:26:56 -0400

    Add chatbox logic code

--- daythree.js
+++ daythree.js
@@ -3,6 +3,7 @@
 console.log("Hello");
 
 const output = document.getElementById("output");
+output.scrollTop = output.scrollHeight
 const inputArea = document.getElementById("inputArea");
 const prompt = document.getElementById("prompt");
 
@@ -42,41 +43,71 @@ async function promptToAPI(url, options) {
     }
 }
 
-function render(response){
-    output.textContent = ""
+function render(response, isBot){
     const p = document.createElement("p");
-    p.textContent = response
-    output.appendChild(p)
+    if (isBot) {
+      p.className =
+        "max-w-[75%] mr-auto rounded-lg bg-gray-300 px-2 py-2 text-black";
+    } else {
+      p.className =
+        "max-w[75%] ml-auto rounded-lg bg-blue-600 px-2 py-2 text-white";
+    }
+    p.textContent = response;
+    output.appendChild(p);
 }
+
+//Global Variable for the collapsible chatbox AI assistant//
+const chatHeader = document.getElementById("chatHeader");
+const chatWindow = document.getElementById("chatWindow");
 
 async function main() {
     try {
-        inputArea.addEventListener("submit", async(event)=>{
-            event.preventDefault()
-            const corsURL = "https://corsproxy.io/?url=";
-            const workersEndpoint = "https://api.cloudflare.com/client/v4/accounts/82bd1d2acadf816a51be4a65b1c64317/ai/run/@cf/meta/llama-3-8b-instruct";
-            const url = corsURL + workersEndpoint
-            const key = await getKey();
-            
-            const promptBody = {
-                messages: [
-                    {role: "system", content: "You are a sales rep trying to make sales on Clarinets, Saxophones, Drums, Guitars, Trumpets, Trombones, and French Horns. You do not go off topic, you only tell the products or info about what a product is. Never respond with more than 15 words, and never list every single product, perhaps ask a question.", },
-                    { role: "user", content: prompt.value },
-                ],
-            };
-            
-            const options = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${key}`,
-                },
-                body: JSON.stringify(promptBody),
-            };
-            const { response } = await promptToAPI(url, options);
-            //const response = "We specialize in woodwind and brass instruments, such as clarinets and trumpets.";
-            render(response)
-        })
+      const messages = [
+        {
+          role: "system",
+          content:
+            "You are a sales rep trying to make sales on Clarinets, Saxophones, Drums, Guitars, Trumpets, Trombones, and French Horns. You do not go off topic, you only tell the products or info about what a product is. Never respond with more than 15 words, and never list every single product, perhaps ask a question?",
+        },
+      ];
+      inputArea.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const corsURL = "https://corsproxy.io/?url=";
+        const workersEndpoint =
+          "https://api.cloudflare.com/client/v4/accounts/82bd1d2acadf816a51be4a65b1c64317/ai/run/@cf/meta/llama-3-8b-instruct";
+        const url = corsURL + workersEndpoint;
+        const key = await getKey();
+
+        messages.push({ role: "user", content: prompt.value });
+        const promptBody = {
+          message: messages,
+        };
+
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${key}`,
+          },
+          body: JSON.stringify(promptBody),
+        };
+        const { response } = await promptToAPI(url, options);
+        messages.push({ role: "assistant", content: response });
+        render(prompt.value, false);
+        render(response, true);
+      });
+      //Collapsible chatbox AI Assistant on DOM//
+      let isOpen = true;
+      chatHeader.addEventListener("click", () => {
+        isOpen = !isOpen;
+
+        if (isOpen) {
+          chatWindow.classList.remove("max-h-[50px");
+          chatWindow.classList.add("max-h-[425px");
+        } else {
+          chatWindow.classList.add("max-h-[50px");
+          chatWindow.classList.remove("max-h-[425px");
+        }
+      });
     } catch (error) {
        console.log(error) 
     }
